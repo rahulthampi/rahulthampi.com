c8df39c9a64db96e00ff29c1aa4f6d41a14b2a67
Rahul Thampi T <trahulthampi@gmail.com>
2020-05-24 16:39:14 +0530

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const RootEl = styled.div``;
+
+const App: React.FC = () => {
+  return <RootEl>{'Hello World!!!'}</RootEl>;
+};
+
+export default App;
